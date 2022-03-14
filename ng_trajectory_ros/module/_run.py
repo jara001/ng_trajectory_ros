da0b106d0e1f8e760aa6a49ac693796a9ec122bd
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-03-14 16:02:41 +0100

    Add callbacks, subscribers and basic functionality

--- ng_trajectory_ros/module/_run.py
+++ ng_trajectory_ros/module/_run.py
@@ -12,7 +12,17 @@ from autopsy.reconfigure import ParameterServer
 # For reading the configuration
 import os
 
+import numpy
 import ng_trajectory
+
+
+# Messages
+from nav_msgs.msg import Path
+from nav_msgs.msg import GridCells
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped
 
 
 ######################
@@ -22,6 +32,8 @@ import ng_trajectory
 class RunNode(Node):
 
     # Node variables
+    start_points = None
+    valid_points = None
     P = ParameterServer()
 
     # Parameters
@@ -35,8 +47,51 @@ class RunNode(Node):
     def __init__(self):
         super(Node, self).__init__("ng_trajectory_ros")
 
+        self.sub_path = self.Subscriber("path", Path, self.callback_path)
+        self.sub_validarea = self.Subscriber("validarea", GridCells, self.callback_validarea)
 
+        self.pub_path = self.Publisher("npath", Path, queue_size = 1, latch = True)
+
+
+    # Callbacks
+    def callback_path(self, msg):
+        """Obtain Path and store it for later usage.
+
+        This path is expected to be sorted. It is used as
+        a baseline for the genetic algorithm.
+
+        Only part of the points from the path is selected.
+        """
+
+        self.start_points = numpy.asarray([ [_p.pose.position.x, _p.pose.position.y ] for _p in msg.poses ])
+
+        print ("Racingline received.")
+
+        if self.valid_points is not None:
+            self.start_optimization(msg.header)
+
+
+    def callback_validarea(self, msg):
+        """Obtain valid grid points and start the optimization.
+
+        Group of all valid points (within the track) is split into groups
+        by the distance to the selected points.
+
+        From each "bin" only one point is selected in the genetic algorithm.
+        """
+
+        self.valid_points = numpy.asarray([ [_p.x, _p.y ] for _p in msg.cells ])
+
+        print ("Valid points received.")
+
+        if self.start_points is not None:
+            self.start_optimization(msg.header)
+
+
+    # Optimization
     def load_config(self):
+        """Load the configuration for ng_trajectory."""
+
         if os.path.isfile(self.P.config_file.value):
             if os.access(self.P.config_file.value, os.R_OK):
                 ng_trajectory.configurationLoad(self.P.config_file.value)
@@ -44,3 +99,32 @@ class RunNode(Node):
                 raise IOError("File '%s' is not readable." % self.P.config_file.value)
         else:
             raise IOError("File '%s' does not exist." % self.P.config_file.value)
+
+
+    def start_optimization(self, header):
+        """Start the optimization and publish its results.
+
+        Arguments:
+        header -- Header class from the messages
+        """
+
+        fitness, rcandidate, tcandidate, result = ng_trajectory.execute(self.start_points, self.valid_points)
+
+        self.pub_path.publish(
+            Path(
+                header = header,
+                poses = [
+                    PoseStamped(
+                        pose = Pose(
+                            position = Point(
+                                point[0], point[1], 0
+                            ),
+                            orientation = Quaternion(
+                                0, 0, 0, 1
+                            ),
+                        )
+                    )
+                    for i, point in enumerate(result)
+                ],
+            )
+        )
